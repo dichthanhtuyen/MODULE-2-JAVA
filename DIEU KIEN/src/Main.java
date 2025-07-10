0ac3e2edf794665552ebfaab3b3b746af4df1e63
dichthanhtuyen <dichthanhtuyen@gmail.com>
2025-07-10 08:27:16 +0700

    dieu kien

---
+++ DIEU KIEN/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Nhập số vào đây");
+        Scanner inputNumber = new Scanner(System.in);
+        int number = inputNumber.nextInt();
+        if (number % 2 == 0) {
+            System.out.println("số "+number+" là số chẵn");
+        }
+        else {
+            System.out.println("Số "+number+" là số lẻ");
+        }
+    }
+}
