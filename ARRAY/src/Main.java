7987bd1b898e8bf74e839cd67dfb55486cb6e9e6
dichthanhtuyen <dichthanhtuyen@gmail.com>
2025-07-11 15:38:22 +0700

    array

---
+++ ARRAY/src/Main.java
@@ -0,0 +1,27 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Arrays;
+import  java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+//        double[] myList = new double[10];
+//        myList[0]=1;
+//        myList[1]=2;
+//        myList[2]=2;
+//        myList[3]=2;
+//        myList[4]=2;
+//        myList[5]=2;
+//        myList[6]=2;
+//        myList[7]=2;
+//        myList[8]=2;
+//        myList[9]=2;
+//        myList[5]=myList[1]+myList[6];
+//        System.out.println(myList.length);
+//        System.out.println(myList[0]);
+//        System.out.println(myList[5]);
+//        int[] array = {0,1,2};
+//        System.out.println(Arrays.toString(array));
+        
+
+    }
+}
